78af329a359eb24fc3ae5d318203292eda23d112
Adhi2019 <59007036+Adhi2019@users.noreply.github.com>
2023-08-30 14:53:58 +0530

    Update s3.tf

--- terraform/Ep_6/modules/s3/s3.tf
+++ terraform/Ep_6/modules/s3/s3.tf
@@ -1,12 +1,12 @@
 # Module for private S3 bucket
 # Author: Somnath Das
 
-resource "aws_s3_bucket" "mys3" {
+resource "aws_s3_bucket" "hga-lmap-s3" {
   bucket = var.bucket_name
   tags   = var.tags
 }
 
 resource "aws_s3_bucket_acl" "acl" {
-  bucket = aws_s3_bucket.mys3.id
+  bucket = aws_s3_bucket.hga-lmap-s3.id
   acl    = "private"
 }
